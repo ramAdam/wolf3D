ba3a876212cd7ad098bcde5667cb37818db6efdc
ram <meerdara@hotmail.com>
2019-10-20 14:41:38 -0400

    added line rotation

---
+++ primitives.py
@@ -0,0 +1,15 @@
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1 
+        self.x2 = x2
+        self.y2 = y2
+
+    def rotate(self, angle):
+        pass
+
+    def draw(self):
+        # pyglet.graphics.draw(2, pyglet.gl.GL_LINES, 
+        # ('v2i', (self.x1))
+        # )
+        pass
